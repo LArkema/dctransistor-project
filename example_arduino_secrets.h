4c1fcffecc583474430b6c42643b7690f89e5507
LArkema <loganarkema@gmail.com>
2021-10-18 14:30:12 -0700

    Clarify real names in example secrets files

--- example_arduino_secrets.h
+++ example_arduino_secrets.h
@@ -1,3 +1,5 @@
+//Real filename should be "arduino_secrets.h" (produced automatically if using Makefile)
+
 #define SECRET_SSID "WiFi_SSID_Name" //Name of your local WiFi network here
 #define SECRET_WIFI_PASS "Wifi Password" //Password for your local WiFi network here
 #define WMATA_API_KEY "0123456789abcdef" //Register a WMATA API key at https://developer.wmata.com/signup/
