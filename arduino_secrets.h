a06d066852173027d8007c74bd5d2b76399fe730
LArkema <loganarkema@gmail.com>
2021-10-18 14:17:04 -0700

    Example / initial secrets files

---
+++ arduino_secrets.h
@@ -0,0 +1,3 @@
+#define SECRET_SSID "example SSID"
+#define SECRET_PASS "WiFi password"
+#define SECRET_WMATA_API_KEY "0123456789abcdef"
---
+++ example_arduino_secrets.h
@@ -0,0 +1,3 @@
+#define SECRET_SSID "WiFi_SSID_Name" //Name of your local WiFi network here
+#define SECRET_WIFI_PASS "Wifi Password" //Password for your local WiFi network here
+#define WMATA_API_KEY "0123456789abcdef" //Register a WMATA API key at https://developer.wmata.com/signup/
