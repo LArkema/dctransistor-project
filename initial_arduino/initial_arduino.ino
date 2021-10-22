03db4556d17f7f7860e72e7b22aefcfb3278aa3e
Logan Arkema <38381313+LArkema@users.noreply.github.com>
2021-10-22 16:32:16 -0400

    Update initial_arduino.ino
    Add comments / minor code cleanup.

--- initial_arduino/initial_arduino.ino
+++ initial_arduino/initial_arduino.ino
@@ -13,24 +13,23 @@
 
 char* ssid = SECRET_SSID;
 const char* password = SECRET_PASS;
-const char* station_fingerprint = "D2 1C A6 D1 BE 10 18 B3 74 8D A2 F5 A2 DE AB 13 7D 07 63 BE";
+const char* wmata_fingerprint = "D2 1C A6 D1 BE 10 18 B3 74 8D A2 F5 A2 DE AB 13 7D 07 63 BE"; //Expires 10/22/2022 
 String wmata_host = "https://api.wmata.com";
 
 ESP8266WiFiMulti WiFiMulti;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  Serial.begin(9600); //NodeMCU ESP8266 runs on 9600 baud rate
 
-  
+  //Limit WiFi to station mode and connect to network defined in arduino_secrets.h
   WiFi.mode(WIFI_STA);
   WiFiMulti.addAP(ssid, password);
-  
+
+  //Connect to WiFi
   while (WiFiMulti.run() != WL_CONNECTED){
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
-
   Serial.println("Wifi Connected");
   
 }
@@ -38,23 +37,26 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
 
+  //Define client responsible for managing secure connection api.wmata.com
   WiFiClientSecure client;
-
-  client.setFingerprint(station_fingerprint);
+  client.setFingerprint(wmata_fingerprint);
   client.setTimeout(15000);
 
   HTTPClient https;
 
+  //Define URL for GET request and confirm correctness
   String station_code = "A01";
   String endpoint = wmata_host + "/StationPrediction.svc/json/GetPrediction/" + station_code;
-
   Serial.println(endpoint);
 
+  //Connect and confirm HTTPS connection to api.wmata.com
   if (https.begin(client, endpoint)) {
+    
+    //If successful, add API key and request station data
     https.addHeader("api_key", SECRET_WMATA_API_KEY);
-
     int httpCode = https.GET();
 
+    //Print out HTTP code and, if succesful, JSON station data
     if (httpCode > 0) {
       String payload = https.getString();
       Serial.println(httpCode);
@@ -63,12 +65,13 @@ void loop() {
     else {
       Serial.println("GET Request failed");
       Serial.println(httpCode);
-    }
-  }
+    }//end httpCode
+    
+  }//END https.begin
 
   else {
     Serial.println("HTTPS connection failed");
   }
 
   delay(3000);
-}
+}//END LOOP
