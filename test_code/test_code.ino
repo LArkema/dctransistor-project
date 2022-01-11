3209d4caef314449e58c7e8dc7c544cd13ba49f1
LArkema <loganarkema@gmail.com>
2022-01-10 17:46:26 -0800

    action test

--- test_code/test_code.ino
+++ test_code/test_code.ino
@@ -9,13 +9,14 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
-#include <
+#include <ArduinoJson.h>
 #include "arduino_secrets.h"
 
 char* ssid = SECRET_SSID;
 const char* password = SECRET_PASS; 
 const char* wmata_fingerprint = "D2 1C A6 D1 BE 10 18 B3 74 8D A2 F5 A2 DE AB 13 7D 07 63 BE"; //Expires 10/22/2022 
 String wmata_host = "https://api.wmata.com";
+String station_endpoint = "/StationPrediction.svc/json/GetPrediction/";
 
 ESP8266WiFiMulti WiFiMulti;
 
@@ -30,6 +31,7 @@ void setup() {
   while (WiFiMulti.run() != WL_CONNECTED){
     delay(1000);
     Serial.println("Connecting to WiFi...");
+    yield();
   }
   Serial.println("Wifi Connected");
   
@@ -45,13 +47,23 @@ void loop() {
 
   HTTPClient https;
 
+/*
+  //String tmpstring = "";
+  const char* direction = new char[16];
+  const char* destination = new char [2];
+  const char* min = new char [4];
+  */
+
+  StaticJsonDocument<500> doc;
+
   //Define URL for GET request and confirm correctness
   String station_code = "A01";
-  String endpoint = wmata_host + "/StationPrediction.svc/json/GetPrediction/" + station_code;
+  String endpoint = wmata_host + station_endpoint + station_code;
 
   //Connect and confirm HTTPS connection to api.wmata.com
   if (https.begin(client, endpoint)) {
-    
+    //Serial.println("In https connection");
+    yield();
     //If successful, add API key and request station data
     https.addHeader("api_key", SECRET_WMATA_API_KEY);
     int httpCode = https.GET();
@@ -59,8 +71,27 @@ void loop() {
     //Print out HTTP code and, if successful, JSON station data
     if (httpCode > 0) {
       String payload = https.getString();
-      Serial.println(httpCode);
-      Serial.println(payload);
+      //Serial.println(payload);
+
+      DeserializationError error = deserializeJson(doc, payload);
+
+      //Relevant JSON variables: Group (1 or 2 - direction) ; Destination (short) / DestinationCode (A13) / DestinationName (full) ; Min (positive int, "ARR" or "BRD")
+      yield();
+      
+      //train = doc["Trains"][0];
+
+      //Serial.println(train["Destination"]);
+
+      const char* direction0 = doc["Trains"][0]["Group"].as<char*>();
+      const char* destination0 = doc["Trains"][0]["Destination"].as<char*>();
+      const char* min0 = doc["Trains"][0]["Min"].as<char*>();
+
+      const char* direction1 = doc["Trains"][1]["Group"].as<char*>();
+      const char* destination1 = doc["Trains"][1]["Destination"].as<char*>();
+      const char* min1 = doc["Trains"][1]["Min"].as<char*>();
+
+      Serial.printf("Direction: %s, towards %s in %s minutes\n", direction0, destination0, min0);
+      Serial.printf("Direction: %s, towards %s in %s minutes\n", direction1, destination1, min1);
     }
     else {
       Serial.println("GET Request failed");
