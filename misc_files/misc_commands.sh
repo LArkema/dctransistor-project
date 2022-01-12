29dba2f99bad69d3270a6d2dc54b549c25b60f2a
LArkema <loganarkema@gmail.com>
2022-01-11 18:13:19 -0800

    Add shell command to get api cert fingerprint

--- misc_files/misc_commands.sh
+++ misc_files/misc_commands.sh
@@ -1,3 +1,6 @@
+# Get WMATA certificate fingerprint in format WiFiClientSecure library expects
+echo | openssl s_client -showcerts -servername api.wmata.com -connect api.wmata.com:443 2>/dev/null | openssl x509 -inform pem -fingerprint -sha1 | head -n 1 | cut -d '=' -f 2 | sed "s/:/ /g"
+
 
 # Get list of stations for development reference
 curl -X GET "https://api.wmata.com/Rail.svc/json/jStations" -H "api_key: $WMATA_KEY" --data-ascii "{body}" > station_list_json.txt
