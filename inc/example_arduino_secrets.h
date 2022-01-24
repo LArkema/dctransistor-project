816d6c741ba4c08772b926c63c49f2fac71b9fe2
Logan Arkema <38381313+LArkema@users.noreply.github.com>
2022-01-23 23:25:23 -0500

    Create example_arduino_secrets.h
    Move headers to inc/ folder

---
+++ inc/example_arduino_secrets.h
@@ -0,0 +1,5 @@
+//Real filename should be "arduino_secrets.h" (produced automatically if using Makefile)
+
+#define SECRET_SSID "WiFi_SSID_Name" //Name of your local WiFi network here
+#define SECRET_PASS "Wifi Password" //Password for your local WiFi network here
+#define SECRET_WMATA_API_KEY "0123456789abcdef" //Register a WMATA API key at https://developer.wmata.com/signup/
