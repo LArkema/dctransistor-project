34cc530f00c1d699e3a93eb914fe74afcedecbad
LArkema <loganarkema@gmail.com>
2022-02-15 19:10:20 -0800

    Add way to set the current state at boot-up in SimpleList. Needs error / bound checking and unit tests.

--- test_code/SimpleList.h
+++ test_code/SimpleList.h
@@ -13,7 +13,7 @@ class SimpleList {
   private:
     uint8_t len;
 
-    //Insert new station to waiting station list to represent new train. New train only added when train arrives at 1st station.
+    //Add new station to waiting station list to represent new train. New train only added when train arrives at 1st station.
     //Since 1st station (index 0) always waiting, insert new train into 2nd position and copy all other values over one.
     //Returns new size of waiting stations list, or -1 if out-of-bounds (list is full)
     int add(){
@@ -36,6 +36,41 @@ class SimpleList {
       } 
     }//end add
 
+    //Insert a station into list of waiting stations using insertion sort. 
+    //Only called when initially setting line state, when order of inserted stations is unknown
+    //TODO: Optimize - insertion sort.
+    int insert(uint8_t station_num){
+      Serial.println("In Insert");
+      Serial.printf("Current State: %s\tCurrent Len: %d\n", getState(), len);
+      if(len == 1){
+        stations[1] = station_num;
+        len++;
+        return 0;
+      }
+
+      else if(station_num > stations[len-1]){
+        stations[len] = station_num;
+        len++;
+        return 0;
+      }
+
+      for(int8_t i = 0; i < len; i++){
+        if(station_num < stations[i]){
+          Serial.printf("Station %d is smaller than station %d. Inserting it at index %d", station_num, stations[i], i);
+          if(len-i > 0){
+            uint8_t* buf = new uint8_t[len-i]; /*Flawfinder: ignore */
+            memcpy(buf, stations+i, (len-i)); /*Flawfinder: ignore */
+            memcpy(stations+i+1, buf, (len-i)); /*Flawfinder: ignore */
+            delete[] buf;
+          }//end shift
+          stations[i] = station_num;
+          len++;
+          return 0;
+        }//end if smaller value.
+      }
+      return -1;
+    }//end insert
+
     //Remove a train after reaching end of line. Removes end of list (i.e. dequeue)
     //Returns size of waiting stations list, 
     //Returns -1 if last train not at end of line
@@ -54,8 +89,6 @@ class SimpleList {
   public:
     uint8_t stations[MAX_NUM_STATIONS]; //list of integers representing station indexes /*Flawfinder: ignore */
     uint8_t last_station = MAX_NUM_STATIONS-1;
-    uint16_t last_circuit_min = 629; //between 629 and 652
-    uint16_t last_circuit_max = 652;
 
     //Constructor sets first station as waiting and initializes rest of list to 0.
     SimpleList(){
@@ -89,6 +122,29 @@ class SimpleList {
       }//end if
       return - 1;
     }//end updates
+
+    //Set current state of waiting stations based on array of active train positions and station circuit numbers.
+    //TODO: Error handling. Make efficient (binary search?)
+    int setInitialState(uint16_t *train_positions, uint8_t train_len, const uint16_t *station_circuits, uint8_t station_len){
+      
+      //Iterate through each active train. If it's between arriving (w/i 2 circuits) at a given station and arriving at the next station,
+      //update state to show it arriving at that station.
+      for(uint8_t i=0; i<train_len; i++){
+        for(uint8_t j=0; j<station_len-1; j++){
+          if(train_positions[i] >= (station_circuits[j] - 2) && train_positions[i] < (station_circuits[j+1] -2)){
+            Serial.println("Updating State");
+            insert(j+1); //Adding to list of waiting stations
+            break;
+          }
+        }//end loop through station circuit list
+      }//end loop through active trains
+
+      return 0;
+    }//end SetState
+
+    uint8_t* getStations(){
+      return stations;
+    }
 
     //Returns state of waiting stations as comma separated list of station indexes
     String getState(){
