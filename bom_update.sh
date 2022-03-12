ffe9e548d76b4642fefdf2b54c04de9cb0e63412
LArkema <loganarkema@gmail.com>
2022-03-12 14:06:38 -0800

    Fix sed error in bom_update

--- bom_update.sh
+++ bom_update.sh
@@ -5,9 +5,9 @@
 ## Update ArduinoJson library version, replacing version in BOM with version Arduino library.properties file.
 OldVer=$(cat bom.json | jq '.components[] | select(.name == "ArduinoJson") | .version' | cut -d'"' -f 2)
 NewVer=$(find ~/ -path "*ArduinoJson*" -name "library.properties" -exec grep "version" {} \; | cut -d '=' -f 2)
-sed -i "/s/$OldVer/$NewVer/" bom.json
+sed -i "s/$OldVer/$NewVer/" bom.json
 
 ## Update ESP8266 core version, replacing version in BOM with version in package path name
 OldVer=$(cat bom.json | jq '.components[] | select(.name == "ESP8266 Arduino Core") | .version' | cut -d'"' -f 2)
 NewVer=$(find ~/ -path "*esp8266*" -name "package.json" | grep -E "esp8266\/[0-9]+.[0-9]+.[0-9]*" -o | cut -d '/' -f 2) #Version is in path name after "esp8266/" and is a series of 2-3 numbers separated by digits (e.g. 3.0.2)
-sed -i "/s/$OldVer/$NewVer/" bom.json
+sed -i "s/$OldVer/$NewVer/" bom.json
