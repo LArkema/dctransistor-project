dd4d2fe6afb0e49467193c7e4e98c3301e46a4b5
LArkema <loganarkema@gmail.com>
2022-03-13 17:17:13 -0700

    Make Direction 1 (negative) work by itself and Fix GitHub Action bug (only push BOM)

--- test_code/miscFuncs.h
+++ test_code/miscFuncs.h
@@ -10,12 +10,14 @@
 //TODO: APPLY OPPOSITE DIRECTION (Negative Circuit traversal) LOGIC
 int checkEndOfLine(TrainLine &line, uint16_t* train_pos, uint8_t train_len, bool dir){
 
+  int8_t coefficient = (dir * -2) + 1; //turns 0 to 1 and 1 to -1
+
   //Only enter if waiting for train at last station or train sitting at last station
   if(line.at(line.getLen(dir)-1, dir) == line.getTotalNumStations()-1 || line.getCyclesAtEnd(dir) != 0){
 
-    //Serial.println("Checking last station");
+    Serial.println("Checking last station");
 
-    //If train reaches end of line, cycles_at_end is non-zero.
+    //If train has reached end of line, cycles_at_end is non-zero.
     if(line.getCyclesAtEnd(dir) > 0){
       Serial.println("Incrementing cycle");
       if(line.incrementCyclesAtEnd(dir) == 0){ //increment rolls over to 0 when max reached
@@ -29,20 +31,19 @@ int checkEndOfLine(TrainLine &line, uint16_t* train_pos, uint8_t train_len, bool
 
     //If train not at end of line, check if is (or isn't)
     for(int i=0; i<train_len; i++){
+
       //Only look at trains arriving at or past 2nd to last station
-      if(train_pos[i] >= line.getStationCircuit(line.getTotalNumStations()-2, dir)-2 ){
+      //Multiply both train position and circuitID 2 circuits before 2nd to last station by coefficient
+      if(train_pos[i]*coefficient >= (line.getStationCircuit(line.getTotalNumStations()-2, dir)*coefficient)-2) {
         
 
         //If a train is between 2nd to last and last station, has not arrived yet. Return.
-        if(train_pos[i] < (line.getLastCID(0) - 2) ){
-          //Serial.println("Train en route to last. Not arrived yet.");
-          //Serial.println(String(train_pos[i]));
+        if(train_pos[i]*coefficient < (line.getLastCID(dir)*coefficient)-2){
+          Serial.printf("Train %d en route to last. Not arrived yet.\n", train_pos[i]);
 
           delete[] possible_trains;
           return -1;
         }
-
-        //Serial.printf("Possible Train: %d\n", train_pos[i]);
 
         possible_trains[possible_trains_len] = train_pos[i];
         possible_trains_len++;
@@ -51,15 +52,18 @@ int checkEndOfLine(TrainLine &line, uint16_t* train_pos, uint8_t train_len, bool
       
     }//end loop through active trains. Check all trains before checking 1st circuit in opp direction.
 
-    //TODO: FIX THIS CODE
+    //Loop through all possible trains, check if it has arrived at last station circuit yet.
     for(uint8_t i=0; i < possible_trains_len; i++){
-      Serial.println(String(possible_trains[i]));
+      Serial.printf("Possible train at last station for direction %d: %d\n", dir, possible_trains[i]);
 
-      if( ( (possible_trains[i] >= (line.getLastCID(0) - 3)) && (possible_trains[i] <= line.getLastCID(0)) ) ||
-       possible_trains[i] == line.getOppCID(0) ){
-        Serial.println("Last station reached");
-        line.arrived(line.getLen(0)-1, 0);
-        line.incrementCyclesAtEnd(0);
+      //Arrived if train between 3 circuits before last station and last station, or at 1st station of other direction.
+      if( ( (possible_trains[i]*coefficient >= (line.getLastCID(dir)*coefficient)-3) 
+       && (possible_trains[i]*coefficient <= line.getLastCID(dir)*coefficient) ) ||
+       possible_trains[i] == line.getOppCID(dir) ){
+
+          Serial.println("Last station reached");
+          line.arrived(line.getLen(dir)-1, dir);
+          line.incrementCyclesAtEnd(dir);
 
         delete[] possible_trains;
         return 0;
--- test_code/test_code.ino
+++ test_code/test_code.ino
@@ -5,34 +5,29 @@
 
 
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
+//#include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
-#include "example_arduino_secrets.h"
+#include "arduino_secrets.h"
 #include "miscFuncs.h" 
 
 
 
-char* ssid = SECRET_SSID;
-const char* password = SECRET_PASS; 
+String ssid = SECRET_SSID;
+String password = SECRET_PASS; 
 const char* wmata_fingerprint = "D2 1C A6 D1 BE 10 18 B3 74 8D A2 F5 A2 DE AB 13 7D 07 63 BE"; //Expires 10/22/2022 
 String wmata_host = "https://api.wmata.com";
 String station_endpoint = "/TrainPositions/TrainPositions?contentType=json";
-
-ESP8266WiFiMulti WiFiMulti;
-
-//Function definition.
-int checkEndOfLine(TrainLine &line, uint16_t* train_pos, uint8_t train_len);
 
 void setup() {
   Serial.begin(9600); //NodeMCU ESP8266 runs on 9600 baud rate
 
   //Limit WiFi to station mode and connect to network defined in example_arduino_secrets.h
-  WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP(ssid, password);
+  //WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
 
   //Connect to WiFi
-  while (WiFiMulti.run() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED){
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
@@ -48,10 +43,12 @@ void loop() {
   client.setFingerprint(wmata_fingerprint);
   client.setTimeout(15000);
 
+
   HTTPClient https;
   https.useHTTP10(true); //enables more efficient Json deserialization per https://arduinojson.org/v6/how-to/use-arduinojson-with-httpclient/
 
   TrainLine redline = TrainLine();
+  bool direction = 1;
 
   //Serial.println(String(redline.getOppCID(0)));
 
@@ -61,7 +58,7 @@ void loop() {
 
   while (true){
 
-    for(int i=redline.getLen(0)-1; i>=0; i--){ //TODO: Move station loop within each API call.
+    for(int i=redline.getLen(direction)-1; i>=0; i--){ //TODO: Move station loop within each API call.
 
       Serial.println(redline.getState());
       uint16_t json_size = 2048;
@@ -92,12 +89,19 @@ void loop() {
          uint16_t j = 0;
 
          for(j=0; j<array_size; j++){ //TODO: replace with Iterators - https://arduinojson.org/v6/api/jsonarray/begin_end/
-           const uint16_t circID = doc["TrainPositions"][j]["CircuitId"].as<unsigned int>();
-           //Add train in test range to list
-           if ( (circID >= redline.getStationCircuit(0,0) && circID <= redline.getStationCircuit(9,0)) || 
-           (circID >= redline.getOppCID(0) && circID < (redline.getOppCID(0) + 2)) ){
+           int8_t coefficient = (direction * -2) + 1; //turns 0 to 1 and 1 to -1
+           const int16_t circID = doc["TrainPositions"][j]["CircuitId"].as<unsigned int>() * coefficient;
 
-             train_positions[k] = circID;
+            int16_t start_circuit = redline.getStationCircuit(0, direction) * coefficient;
+            int16_t end_circuit = redline.getStationCircuit(9, direction) * coefficient;
+
+            //Serial.printf("Circuit %d must be greater than %d and less than %d\n", circID, start_circuit, end_circuit);
+
+           //Add train in test range to list
+           if ( (circID >= start_circuit && circID <= end_circuit) || 
+           (circID >= redline.getOppCID(direction) * coefficient && circID < (redline.getOppCID(direction) + 2)*coefficient) ){
+
+             train_positions[k] = circID * coefficient;
              k++;
            }
          }
@@ -108,31 +112,31 @@ void loop() {
           }
           Serial.println();
 
-          if (redline.getLen(0) < 2){
+          if (redline.getLen(1) < 2){
             Serial.println("Setting initial state");
-            redline.setInitialStations(train_positions, k, 0);
+            redline.setInitialStations(train_positions, k, direction);
             Serial.println(redline.getState());
           }
 
           //Put target station in local circuit
-          uint16_t station_circuit = redline.getWaitingStationCircuit(i, 0); //redline stores station indexes.
+          uint16_t station_circuit = redline.getWaitingStationCircuit(i, direction); //redline stores station indexes.
 
           //See if any of the current train positions are arriving, at, or just leaving destination station.
           //If so, update station state.
           for(int t=0; t<k; t++){
-            if(train_positions[t] > (station_circuit - 3) && train_positions[t] < (station_circuit + 1) 
-            && station_circuit != redline.getLastCID(0) ){
+            if(train_positions[t] > (station_circuit - 3) && train_positions[t] < (station_circuit + 3) 
+            && station_circuit != redline.getLastCID(direction) ){
 
               Serial.printf("Station Circuit: %d\n", station_circuit); /*Flawfinder: ignore */
               Serial.printf("Train Circuit:   %d\n", train_positions[t]); /*Flawfinder: ignore */
 
-              redline.arrived(i, 0);
+              redline.arrived(i, direction);
 
               break;
             }
           }
 
-          checkEndOfLine(redline, train_positions, k, 0);
+          checkEndOfLine(redline, train_positions, k, direction);
           
 
         }
