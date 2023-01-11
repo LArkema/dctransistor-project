37c2f5cd86161650a81b632af5cefeb6b92a8a3c
Logan Arkema <38381313+LArkema@users.noreply.github.com>
2023-01-10 21:46:00 -0500

    Create arduino_secrets.h
    remove "example_" from file name

---
+++ test_code/arduino_secrets.h
@@ -0,0 +1,3 @@
+#define SECRET_SSID "WiFi_SSID_Name" //Name of your local WiFi network here
+#define SECRET_PASS "Wifi Password" //Password for your local WiFi network here
+#define SECRET_WMATA_API_KEY "0123456789abcdef" //Register a WMATA API key at https://developer.wmata.com/signup/
