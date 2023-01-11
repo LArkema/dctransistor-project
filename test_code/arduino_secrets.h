851696e1ec795556bf3f0f43cc74133b9c3fdda1
LArkema <loganarkema@gmail.com>
2023-01-10 19:12:08 -0800

    remove arduino_secrets from test_code

--- test_code/arduino_secrets.h
+++
@@ -1,3 +0,0 @@
-#define SECRET_SSID "WiFi_SSID_Name" //Name of your local WiFi network here
-#define SECRET_PASS "Wifi Password" //Password for your local WiFi network here
-#define SECRET_WMATA_API_KEY "0123456789abcdef" //Register a WMATA API key at https://developer.wmata.com/signup/
