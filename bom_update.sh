aacc5977029e60ccb9c5d586ef21e96810f4588a
LArkema <loganarkema@gmail.com>
2023-01-10 20:17:04 -0800

    moar debug

--- bom_update.sh
+++ bom_update.sh
@@ -6,28 +6,30 @@
 OldVer=$(cat bom.json | jq '.components[] | select(.name == "ArduinoJson") | .version' | cut -d'"' -f 2)
 NewVer=$(find ~/ -path "*ArduinoJson*" -name "library.properties" -exec grep "version" {} \; | cut -d '=' -f 2)
 
-echo $OldVer >&2
-echo $NewVer >&2
-
-sed "s|$OldVer|$NewVer|g" bom.json
-
-echo "After first check" >&2
+sed -i "s/$OldVer|$NewVer/g" bom.json
 
 ## Update LED Backpack library version
 OldVer=$(cat bom.json | jq '.components[] | select(.name == "Adafruit LED Backpack") | .version' | cut -d'"' -f 2)
 NewVer=$(find ~/ -path "*Adafruit_LED_Backpack_Library*" -name "library.properties" -exec grep "version" {} \; | cut -d '=' -f 2)
 
-echo $OldVer
-echo $NewVer
-
-sed "s|$OldVer|$NewVer|g" bom.json
+sed -i "s|$OldVer|$NewVer|g" bom.json
 
 ## Update NeoPixel library version
 OldVer=$(cat bom.json | jq '.components[] | select(.name == "Adafruit NeoPixel") | .version' | cut -d'"' -f 2)
 NewVer=$(find ~/ -path "*Adafruit_LED_Backpack_Library*" -name "library.properties" -exec grep "version" {} \; | cut -d '=' -f 2)
-sed -i "s/$OldVer/$NewVer/" bom.json
+
+echo $OldVer
+echo $NewVer
+
+sed "s|$OldVer|$NewVer|" bom.json
 
 ## Update ESP8266 core version, replacing version in BOM with version in package path name
 OldVer=$(cat bom.json | jq '.components[] | select(.name == "ESP8266 Arduino Core") | .version' | cut -d'"' -f 2)
 NewVer=$(find ~/ -path "*esp8266*" -name "package.json" | grep -E "esp8266\/[0-9]+.[0-9]+.[0-9]*" -o | cut -d '/' -f 2) #Version is in path name after "esp8266/" and is a series of 2-3 numbers separated by digits (e.g. 3.0.2)
-sed -i "s/$OldVer/$NewVer/" bom.json
+
+echo $OldVer
+echo $NewVer
+
+sed -i "s|$OldVer|$NewVer|" bom.json
+
+echo "past Esp8266 version"
